d03008dfe7852c2b293ccd1405ca830ccf5681e1
Sofía Lancuba <sofia@tixcode.com.ar>
2021-01-08 17:25:01 -0300

    create how it works

--- js/script.js
+++ js/script.js
@@ -49,51 +49,6 @@
   });
 
   /*-------------------------------------------------------------------------------
-	  Pagepiling
-	-------------------------------------------------------------------------------*/
-
-  if ($(".a-pagepiling").length) {
-    $(".a-pagepiling").pagepiling({
-      scrollingSpeed: 280,
-      menu: "#menu, #menuMain",
-      anchors: [
-        "Intro",
-        "Assess",
-        "Library",
-        "Teacher",
-        // "Experience",
-        // "Clients",
-        // "Testimonials",
-        // "Contact",
-      ],
-      loopTop: false,
-      loopBottom: false,
-      navigation: {
-        position: "right",
-      },
-      onLeave: function () {
-        $(".header").removeClass("header-shadow");
-        if ($(".pp-scrollable.active").scrollTop() > 0) {
-          $(".header").addClass("header-shadow");
-        } else {
-          $(".header").removeClass("header-shadow");
-        }
-        if ($(".slide-dark-footer").hasClass("active")) {
-          $("body").addClass("body-copyright-light");
-        } else {
-          $("body").removeClass("body-copyright-light");
-        }
-        if ($(".slide-dark-bg").hasClass("active")) {
-          $("body").addClass("body-bg-dark");
-        } else {
-          $("body").removeClass("body-bg-dark");
-        }
-        $(".a-carousel-projects").trigger("refresh.owl.carousel");
-      },
-    });
-  }
-
-  /*-------------------------------------------------------------------------------
 	  Carousels
 	-------------------------------------------------------------------------------*/
 
